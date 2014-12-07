5a8727a907b40abf9769726e149329e0afe06825
Sergey <sergey.shpak.web@gmail.com>
2014-12-07 15:19:02 +0200

    Plugin functionality

---
+++ gulp-dotjsify.js
@@ -0,0 +1,39 @@
+var through = require('through2'),
+    gutil = require('gulp-util'),
+    doT = require('dot'),
+    wrapper = {
+        amd: function (module) {
+            return 'define(["exports"], function (exports) { exports =  \n' + module.toString() + ' });';
+        },
+        cjs: function (module) {
+            return 'module.exports = ' + module.toString();
+        },
+        es6: function (module) {
+            return 'export default function () { return ' + module.toString() + ' };';
+        }
+    };
+
+module.exports = function precompile(options) {
+
+    return through.obj(function (file, enc, cb) {
+
+        if (file.isNull())
+            return cb();
+        if (file.isStream())
+            return cb(new Error(PLUGIN_NAME + ': streaming is not supported'));
+
+        file.path = gutil.replaceExtension(file.path, '.js');
+
+        file.contents = new Buffer(
+            wrapper[options.type](
+                doT.template(
+                    file.contents.toString()
+                )
+            )
+        );
+
+        this.push(file);
+
+        cb();
+    })
+}
